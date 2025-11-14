70ca1714a429b01e40c694f175b441739be0658e
José Gabriel Quintana Ramos <quintanacuevas90@gmail.com>
2025-11-13 20:47:45 -0400

    Change root element ID from 'root' to 'raíz'

--- index.tsx
+++ index.tsx
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
+const rootElement = document.getElementById('raíz');if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
